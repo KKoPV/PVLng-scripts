4df0aafaf152412965ddaca2cb49fc52b37bff18
Knut Kohl <github@knutkohl.de>
2017-05-27 16:33:09 +0200

    Fix copy2ram script

--- copy2ram.sh
+++ copy2ram.sh
@@ -3,14 +3,16 @@
 #set -x
 
 pwd=$(readlink -f $(dirname $0))
+tmp=$1
 
-[ -z "$1" ] && printf "\nUsage: $0 <temp. dir>\n\n" && exit 1
+[ -z "$tmp" ] && printf "\nUsage: $0 <temp. dir>\n\n" && exit 1
 
-rsync -a "$pwd" "$1" --exclude .git --exclude data
+rsync -a "$pwd" "$tmp" --exclude .git --exclude data
 
 echo
 echo "Put this to your crontab:"
-echo "    @reboot $pwd/$(basename $0) $1"
+echo "    @reboot $pwd/$(basename $0) $tmp"
+echo
 echo "Run your scripts in crontab with this base dir:"
-echo "    $pwd"
+echo "    $tmp/"
 echo
